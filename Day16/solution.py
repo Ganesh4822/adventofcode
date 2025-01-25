06142c0fde93f5b46b6f2b366783f40b7b3e23eb
Ganesh <ganesh.ingale@helpshift.com>
2025-01-25 18:44:29 +0530

    added day 16 solution part 1

--- Day16/solution.py
+++ Day16/solution.py
@@ -6,7 +6,6 @@ import heapq
 #for anticlockwise movement move row , col = -col , row 
 
 
-
 #following code snippet follows djiktras algorithm to calculate the shortest path
 #We store (cost to reach the point , coordimates , directions)
 #we start from the starting point as it has cost = 0
@@ -20,20 +19,19 @@ def part_one(grid,rows,cols,rs,cs):
     pq = [(0,rs,cs,0,1)] #(cost, row , col , next_r , next_col)
 
     seen = {(rs,cs,0,1)}
-    tiles = set()
     while pq:
-        cur_cost,r,c,dr,dc = heapq.heappop(pq)
+        cost,r,c,dr,dc = heapq.heappop(pq)
         seen.add((r,c,dr,dc))
-        tiles.add((r,c))
         if grid[r][c] == "E":
-            return len(tiles)
-        for next_cost, nr,nc,ndr,ndc in [(cur_cost + 1, r  + dr , c  + dc , dr , dc),
-                                         (cur_cost + 1000, r  , c  , dc , -dr),
-                                         (cur_cost + 1000, r  , c  , -dc , dr)]:
+            return cost
+        for new_cost,nr,nc,dr,dc in [(cost + 1,r + dr , c + dc , dr,dc), (cost + 1000, r,c ,dc ,-dr), (cost + 1000, r,c ,-dc ,dr)]:
             if grid[nr][nc] == "#":continue
-            if(nr,nc,ndr,ndc ) in seen:continue
-            heapq.heappush(pq,(next_cost, nr,nc,ndr,ndc))
- 
+            if (nr,nc,dr,dc) in seen: continue
+            heapq.heappush(pq,(new_cost,nr,nc,dr,dc))
+
+def part_two(grid,rs,cs):
+    pq = [{0,rs,cs,0,1}]
+
 def main():
     path = "/Users/ganesh.ingale/adventofcode/Day16/example.txt"
     
